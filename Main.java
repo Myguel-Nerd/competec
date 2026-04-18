3d900485a4c00832e40d1a2631a820bacd38df78
Myguel-Nerd <mygueluzumaki@gmail.com>
2026-04-18 11:43:21 -0300

    aula_1_Competec
    código de preço de frutas.

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+    float laranja = 15.90f;
+    float uva = 3.50f;
+    float totalDasFrutas = laranja + uva;
+    
+    System.out.printf("O preço total: R$%.2f%n", totalDasFrutas);
+    
+        
+    }
+}
